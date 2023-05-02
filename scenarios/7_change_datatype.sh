14fed010f75ec440068403dfdc9b932a107ab16b
Valerie Parham-Thompson <16852807+dataindataout@users.noreply.github.com>
2023-05-02 16:56:10 -0400

    Changes to drop table and change datatype scenarios for 2.17

--- scenarios/7_change_datatype.sh
+++ scenarios/7_change_datatype.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
 # file: change_datatype.sh
 
-# get universe IDs and replication name
-eastid=$(curl -s http://127.0.0.1:7000/cluster-config | grep cluster_uuid | awk -F '"' '{print $2}')
-westid=$(curl -s http://127.0.0.4:7000/cluster-config | grep cluster_uuid | awk -F '"' '{print $2}')
-replication_name="milliontable"
-
-# stop replication
-yb-admin --master_addresses 127.0.0.1:7100,127.0.0.2:7100,127.0.0.3:7100 set_universe_replication_enabled $westid"_"$replication_name 0
-yb-admin --master_addresses 127.0.0.4:7100,127.0.0.5:7100,127.0.0.6:7100 set_universe_replication_enabled $eastid"_"$replication_name 0
+# you don't have to stop replication for this one
+# so I'm not doing that here, as an example
 
 # alter datatype on both sides
 ysqlsh -h 127.0.0.1 -c "alter table milliontable alter column name type varchar(25)"
 ysqlsh -h 127.0.0.4 -c "alter table milliontable alter column name type varchar(25)"
-
-# start replication
-yb-admin --master_addresses 127.0.0.1:7100,127.0.0.2:7100,127.0.0.3:7100 set_universe_replication_enabled $westid"_"$replication_name 1
-yb-admin --master_addresses 127.0.0.4:7100,127.0.0.5:7100,127.0.0.6:7100 set_universe_replication_enabled $eastid"_"$replication_name 1
 
 # add a row
 ysqlsh -h 127.0.0.1 -c "INSERT INTO milliontable (name, age) SELECT substr(md5(random()::text), 1, 10), (random() * 70 + 10)::integer"
@@ -30,6 +20,7 @@ ysqlsh -h 127.0.0.4 -c "select count(*) from milliontable"
 # check replication
 echo "replication status:"
 curl "http://127.0.0.1:9000/prometheus-metrics" --silent | grep async_replication
+yb-admin --master_addresses 127.0.0.1:7100,127.0.0.2:7100,127.0.0.3:7100 get_replication_status
 yb-admin --master_addresses 127.0.0.4:7100,127.0.0.5:7100,127.0.0.6:7100 get_replication_status
 
 # verify
