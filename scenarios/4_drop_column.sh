edeb25919103358c4fc01685f457a92e72bce16f
Valerie Parham-Thompson <16852807+dataindataout@users.noreply.github.com>
2023-05-02 16:47:40 -0400

    Change add/drop column scenarios for 2.17

--- scenarios/4_drop_column.sh
+++ scenarios/4_drop_column.sh
@@ -6,16 +6,14 @@ eastid=$(curl -s http://127.0.0.1:7000/cluster-config | grep cluster_uuid | awk 
 westid=$(curl -s http://127.0.0.4:7000/cluster-config | grep cluster_uuid | awk -F '"' '{print $2}')
 replication_name="milliontable"
 
-# stop replication
 # no longer need to stop replication before DDL in 2.17+ because it will pause automatically when ddl is only issued on one side
-# yb-admin --master_addresses 127.0.0.1:7100,127.0.0.2:7100,127.0.0.3:7100 set_universe_replication_enabled $westid"_"$replication_name 0
-# yb-admin --master_addresses 127.0.0.4:7100,127.0.0.5:7100,127.0.0.6:7100 set_universe_replication_enabled $eastid"_"$replication_name 0
 
 # drop column on one side 
 ysqlsh -h 127.0.0.1 -c "alter table milliontable drop column if exists joindate"
 
-# check that replication has been paused
-echo "replication status (should be paused):"
+# check that replication has been paused from east to west
+# expect a schema mismatch error for one of these
+echo "replication status:"
 curl "http://127.0.0.1:9000/prometheus-metrics" --silent | grep async_replication
 yb-admin --master_addresses 127.0.0.1:7100,127.0.0.2:7100,127.0.0.3:7100 get_replication_status
 yb-admin --master_addresses 127.0.0.4:7100,127.0.0.5:7100,127.0.0.6:7100 get_replication_status
